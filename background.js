608c53ac9fcc622a14e9551b6356d058b4248beb
Chris Dzoba <chris@dzoba.com>
2017-06-29 19:32:39 -0700

    Local storage, regex matching, background processing.  Still need to inject div above content.

--- background.js
+++ background.js
@@ -1,3 +1,30 @@
-setInterval(function() {
-  console.log('interval is running');
-}, 500);
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.tabs.onUpdated.addListener(
+    function(tabId, changeInfo, tab) {
+      console.log('acting', tab.url)
+      if (tab.url){
+        var urlColorArray = window.localStorage.getItem('colorPrefs');
+        var split = urlColorArray.split(/\n|,/);
+        var newObj = {};
+        for ( var i = 0; i < split.length; i+=2) {
+          newObj[split[i]] = split[i+1];
+        }
+
+        console.log('hey there', newObj)
+        Object.keys(newObj).forEach(function(key) {
+          console.log('checking if there is a match', key, tab.url)
+          if (tab.url.match(key)) {
+            console.log('there is a match!');
+            chrome.tabs.executeScript(tabId,
+              {
+                code:"document.body.style.border='10px solid " + newObj[key] + "'"
+              }
+            );
+          }
+        });
+
+      }
+
+    }
+  );
+});
--- popup.js
+++ popup.js
@@ -1,48 +1,12 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-var textArea = document.querySelector('textarea');
-var localStorage = window.localStorage;
-
-if (localStorage.colorPrefs) {
-  textArea.value = localStorage.colorPrefs;
-}
-
-textArea.on('change', function(event) {
-  console.log('event', event);
-})
-
-function click(e) {
-  chrome.tabs.executeScript(null,
-      {code:"document.body.style.border='10px solid " + e.target.id + "'"});
-
-}
-
 document.addEventListener('DOMContentLoaded', function () {
-  var divs = document.querySelectorAll('div');
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', click);
+  var localStorage = window.localStorage;
+  var textArea = document.querySelector('textarea');
+
+  if (localStorage.getItem('colorPrefs')) {
+    textArea.value = localStorage.getItem('colorPrefs');
   }
+
+  textArea.addEventListener('input', function(e) {
+    localStorage.setItem('colorPrefs', e.target.value)
+  }, false);
 });
-
-
-chrome.tabs.onUpdated.addListener(
-  function(tabId, changeInfo, tab) {
-    if (tab.url){
-      var urlColorArray = document.querySelector('textarea').value;
-      var split = urlColorArray.split(/\n|,/);
-      console.log('split', split)
-      var newObj = {};
-      for ( var i = 0; i < split.length; i+=2) {
-        newObj[split[i]] = split[i+1];
-      }
-      console.log('urlColorArray', newObj);
-
-    }
-
-  }
-);
-
-
-chrome.tabs.executeScript(null,
-    {code:"document.body.style.border='10px solid red'"});
