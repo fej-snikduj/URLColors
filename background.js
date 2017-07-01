fc3c0ca9aebcfb95401ef9c15cb5a0f6cbf5a583
Jeff Judkins <judkinsj@gmail.com>
2017-07-01 13:33:17 -0700

    add option to deactivate and activate

--- background.js
+++ background.js
@@ -20,72 +20,74 @@ var removePreviousDivs = function(tabId) {
   });
 }
 
+var addDivsToPage = function(tabId, tab) {
+  var prefs = JSON.parse(window.localStorage.getItem('urlColorPrefs'));
+  if (tab.url && prefs.active){
+    var urlColorPairs = prefs.urlColorPairs || '';
+    var opacity = prefs.opacity || .2;
+    var borderWidth = prefs.borderWidth || '15px';
+    var split = urlColorPairs.split(/\n|,/);
+    var newObj = {};
+    for ( var i = 0; i < split.length; i+=2) {
+      newObj[split[i]] = split[i+1];
+    }
+
+    Object.keys(newObj).forEach(function(key) {
+      if (tab.url.match(key)) {
+        log('URL Color Message: Key matches settings - adding divs.')
+        chrome.tabs.executeScript(tabId,
+          {
+            code:`
+              var leftDiv = document.createElement('div');
+              var rightDiv = document.createElement('div');
+              var topDiv = document.createElement('div');
+              var bottomDiv = document.createElement('div');
+
+              var divs = [leftDiv, rightDiv, topDiv, bottomDiv];
+              var horizontal = [topDiv, bottomDiv];
+              var vertical = [rightDiv, leftDiv];
+
+              divs.forEach(function(div, index) {
+                div.setAttribute('class', 'colordiv');
+                div.style.background = '${newObj[key]}';
+                div.style.position = 'fixed';
+                div.style.opacity = ${opacity};
+                div.style.zIndex = '99999999999999';
+              });
+
+              horizontal.forEach(function(div) {
+                div.style.left = '0';
+                div.style.right = '0';
+                div.style.height = '${borderWidth}';
+              });
+
+              vertical.forEach(function(div) {
+                div.style.top = '0';
+                div.style.bottom = '0';
+                div.style.width = '${borderWidth}';
+              });
+
+              leftDiv.style.left = '0';
+              rightDiv.style.right = '0';
+              topDiv.style.top = '0';
+              bottomDiv.style.bottom = '0';
+
+              divs.forEach(function(div) {
+                document.body.appendChild(div);
+              });
+            `
+          }
+        );
+      }
+    });
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   chrome.tabs.onUpdated.addListener(
     function(tabId, changeInfo, tab) {
       removePreviousDivs(tabId);
-      if (tab.url){
-        var prefs = JSON.parse(window.localStorage.getItem('urlColorPrefs'));
-        var urlColorPairs = prefs.urlColorPairs || '';
-        var opacity = prefs.opacity || .2;
-        var borderWidth = prefs.borderWidth || '15px';
-        var split = urlColorPairs.split(/\n|,/);
-        var newObj = {};
-        for ( var i = 0; i < split.length; i+=2) {
-          newObj[split[i]] = split[i+1];
-        }
-
-        Object.keys(newObj).forEach(function(key) {
-          if (tab.url.match(key)) {
-            log('URL Color Message: Key matches settings - adding divs.')
-            chrome.tabs.executeScript(tabId,
-              {
-                code:`
-                  var leftDiv = document.createElement('div');
-                  var rightDiv = document.createElement('div');
-                  var topDiv = document.createElement('div');
-                  var bottomDiv = document.createElement('div');
-
-                  var divs = [leftDiv, rightDiv, topDiv, bottomDiv];
-                  var horizontal = [topDiv, bottomDiv];
-                  var vertical = [rightDiv, leftDiv];
-
-                  divs.forEach(function(div, index) {
-                    div.setAttribute('class', 'colordiv');
-                    div.style.background = '${newObj[key]}';
-                    div.style.position = 'fixed';
-                    div.style.opacity = ${opacity};
-                    div.style.zIndex = '99999999999999';
-                  });
-
-                  horizontal.forEach(function(div) {
-                    div.style.left = '0';
-                    div.style.right = '0';
-                    div.style.height = '${borderWidth}';
-                  });
-
-                  vertical.forEach(function(div) {
-                    div.style.top = '0';
-                    div.style.bottom = '0';
-                    div.style.width = '${borderWidth}';
-                  });
-
-                  leftDiv.style.left = '0';
-                  rightDiv.style.right = '0';
-                  topDiv.style.top = '0';
-                  bottomDiv.style.bottom = '0';
-
-                  divs.forEach(function(div) {
-                    document.body.appendChild(div);
-                  });
-                `
-              }
-            );
-          }
-        });
-
-      }
-
+      addDivsToPage(tabId, tab);
     }
   );
 });
--- popup.js
+++ popup.js
@@ -1,30 +1,63 @@
-var updateLocalStorage = function(property, value) {
+window.urlColors = window.urlColors || {};
+
+urlColors.updateLocalStorage = function(property, value) {
   let storage = JSON.parse(localStorage.getItem('urlColorPrefs') || "{}");
   storage[property] = value;
   localStorage.setItem('urlColorPrefs', JSON.stringify(storage));
 };
+
+urlColors.updateTabs = function() {
+  chrome.tabs.query({}, function(tabArray) {
+    tabArray.forEach((tab) => {
+      chrome.extension.getBackgroundPage().removePreviousDivs(tab.id);
+      chrome.extension.getBackgroundPage().addDivsToPage(tab.id, tab);
+    });
+  });
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   var localStorage = window.localStorage;
   var urlColorPairs = document.querySelector('textarea');
   var opacityInput = document.querySelector('input[name="opacity"]');
   var borderWidthInput = document.querySelector('input[name="border-width"]')
+  var active = document.querySelector('input[name="active"]')
 
   var prefs = JSON.parse(localStorage.getItem('urlColorPrefs') || "{}");
   urlColorPairs.value = prefs.urlColorPairs || '';
   opacityInput.value = prefs.opacity || .2;
   borderWidthInput.value = prefs.borderWidth || '15px';
+  active.checked = typeof(prefs.active) === 'boolean' ? prefs.active : true;
 
   urlColorPairs.addEventListener('input', function(e) {
-    updateLocalStorage('urlColorPairs', e.target.value);
+    urlColors.updateLocalStorage('urlColorPairs', e.target.value);
+    urlColors.updateTabs();
   }, false);
 
   opacityInput.addEventListener('input', function(e) {
-    updateLocalStorage('opacity', e.target.value);
+    urlColors.updateLocalStorage('opacity', e.target.value);
+    urlColors.updateTabs();
   }, false);
 
   borderWidthInput.addEventListener('input', function(e) {
-    updateLocalStorage('borderWidth', e.target.value);
+    urlColors.updateLocalStorage('borderWidth', e.target.value);
+    urlColors.updateTabs();
+  }, false);
+
+  active.addEventListener('change', function(e) {
+    urlColors.updateLocalStorage('active', e.target.checked);
+    if (!e.target.checked) {
+      chrome.tabs.query({}, function(tabArray) {
+        tabArray.forEach((tab) => {
+          chrome.extension.getBackgroundPage().removePreviousDivs(tab.id);
+        });
+      });
+    } else {
+      chrome.tabs.query({}, function(tabArray) {
+        tabArray.forEach((tab) => {
+          chrome.extension.getBackgroundPage().addDivsToPage(tab.id, tab);
+        });
+      });
+    }
   }, false);
 
 });
